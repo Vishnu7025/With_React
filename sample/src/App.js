0bc00df722e4775f27981fc1f4d32d284ee7ef24
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-10 19:34:24 +0530

    no

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,21 +1,11 @@
-import Header from './components/Header'
 function App() {
-  const data = 'Vishnu V.S'
+  let count = 0;
   return (
     <div> 
-      <Header data={data} />
-      <h1 style={{color:'red'}}>heooe world</h1>
-    <p>This is discription {data}</p>
-    
-      <Hello/>
-      <Hello />
-      <Hello />
+    <button>ADD</button>
+      <h1>Counter: {count}</h1>
     </div>
   );
 }
 
 export default App;
-
-function Hello(){
-  return <h1  style={{ backgroundColor:'red'}}>hello world</h1>
-}
--- sample/src/components/Header.js
+++
@@ -1,5 +0,0 @@
-function Header(props){
-    return(<h1>Hello Iam {props.data}</h1>)
-}
-
-export default Header
