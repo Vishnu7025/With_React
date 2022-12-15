31a1a53de686a335eac963046990f260dbe19cdd
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-15 10:58:53 +0530

    useEffect

--- sample/src/Counter.js
+++ sample/src/Counter.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
+import  { useState,useEffect } from 'react'
 
 function Counter() {
-    const [count,setCount] = useState(0)
-  return (
-    <div>
-          <button onClick={() => setCount(count + 1)}  >Increment</button>
-        <h1>Heloo iam component:{count}</h1>
-    </div>
-  )
+    const [count, setCount] = useState(0)
+    useEffect(()=>{
+        console.log('Mounting...'); 
+        return ()=>{
+            console.log('unMounting....');
+            console.log('updating..'+count);
+        }       
+    },[count])
+
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)} >Increment</button>
+            <h1>Heloo iam component:{count}</h1>
+        </div>
+    )
 }
 
 export default Counter
