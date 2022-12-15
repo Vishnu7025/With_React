1b1dd3e8d28c3d115c48dcfaa80c42833682ae63
Vishnu  V S <vishnuviswambaran2002@gmail.com>
2022-12-15 18:39:20 +0530

    todo listing

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,10 +1,38 @@
-  
+import './App.css';
+import {useState} from 'react'
 function App() {
+  const [toDos,setTodos] = useState([])
+  const [toDo,setTodo] = useState('')
   return (
-    <div className="App">
-      <h1>heoo</h1>
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDo} onChange={(e)=>setTodo(e.target.value)} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setTodos([...toDos,toDo])} className="fas fa-plus"></i>
+      </div>
+      <div className="todos">
+       { toDos.map((value)=>{
+
+          return (  <div className="todo">
+                <div className="left">
+                  <input type="checkbox" name="" id="" />
+                  <p>{value}</p>
+                </div>
+                <div className="right">
+                  <i className="fas fa-times"></i>
+                </div>
+              </div> )
+        })}
+      </div>
     </div>
   );
 }
+
 
 export default App;
